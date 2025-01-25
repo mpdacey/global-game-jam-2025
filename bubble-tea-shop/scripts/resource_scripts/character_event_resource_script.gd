5be3641b257b53d48723a40737186f5597b228c7
mpdacey <80190692+mpdacey@users.noreply.github.com>
2025-01-25 14:53:39 +1100

    Removed liquid ratio resource
    decided against mixing liquids

--- bubble-tea-shop/scripts/resource_scripts/character_event_resource_script.gd
+++ bubble-tea-shop/scripts/resource_scripts/character_event_resource_script.gd
@@ -1,7 +1,14 @@
 extends Resource
 class_name CharacterEventResource
 
+enum LiquidFlavour {
+	PEACH,
+	MATCHA,
+	COCONUT,
+	STRAWBERRY
+}
+
 @export var bubbles: Array[BubbleRatio]
-@export var liquids: Array[LiquidRatio]
+@export var liquid: LiquidFlavour
 @export var character_sprite: Texture2D
 @export var dialogue: DialogueResource
