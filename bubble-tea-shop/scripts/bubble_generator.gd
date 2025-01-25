885a1e209054433d2085e3573be4e1566bd853ef
mpdacey <80190692+mpdacey@users.noreply.github.com>
2025-01-25 23:05:41 +1100

    Added max bubble count

--- bubble-tea-shop/scripts/bubble_generator.gd
+++ bubble-tea-shop/scripts/bubble_generator.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+signal max_achieved
+
+const BUBBLE_MAX_COUNT = 40
+
 @export var bubble_colours: Array[Color]
 var bubble_scene: PackedScene = preload("res://scenes/bubble.tscn")
 var bubble_shared_material: ShaderMaterial = preload("res://materials/bubble_material.tres")
@@ -18,6 +22,11 @@ func _on_button_pressed(flavour: BubbleRatio.BubbleFlavour):
 func _bubble_spawn_time_out():
 	var bubble = bubble_scene.instantiate()
 	bubble_collection.add_child(bubble)
+	
+	if bubble_collection.get_child_count() >= BUBBLE_MAX_COUNT:
+		max_achieved.emit()
+		return
+	
 	bubble_spawn_timer.start()
 
 func _on_button_released():
