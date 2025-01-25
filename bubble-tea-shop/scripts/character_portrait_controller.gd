8af20ca9dd012837cc47cac1ef8e1ce64889f28e
mpdacey <80190692+mpdacey@users.noreply.github.com>
2025-01-25 18:20:04 +1100

    Created character portrait controller

---
+++ bubble-tea-shop/scripts/character_portrait_controller.gd
@@ -0,0 +1,23 @@
+extends Sprite2D
+
+signal character_entered
+signal character_exited
+
+@onready var character_animation_tree: AnimationTree = $CharacterAnimationTree
+
+func _input(event):
+	if event.is_action_pressed("test_customer_leave"):
+		exit_customer()
+
+func enter_customer(customer_sprite: Texture2D):
+	self.texture = customer_sprite
+	character_animation_tree.get("parameters/playback").travel("enter")
+
+func exit_customer():
+	character_animation_tree.get("parameters/playback").travel("exit")
+
+func _emit_entered():
+	character_entered.emit()
+
+func _emit_exited():
+	character_exited.emit()
